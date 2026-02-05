8d9644a9d2b2cc07ce997dee05b5a8df5587f125
Rhys Yim <79439703+rhysyim@users.noreply.github.com>
2026-02-05 22:54:06 +0000

    Refactor thermistor instrumentation code

--- Code/Thermistor_Instrumentation/Thermistor_Instrumentation.ino
+++ Code/Thermistor_Instrumentation/Thermistor_Instrumentation.ino
@@ -3,6 +3,7 @@
 #include <math.h>
 
 Adafruit_ADS1015 ads;  // Create ADS1015 object
+Adafruit_ADS1015 ads1;  // Create ADS1015 object
 
 // Thermistor constants
 const float R_SERIES = 10000.0;
@@ -34,7 +35,10 @@ float adcToTemp(int16_t adcValue) {
 
 void setup() {
   Serial.begin(9600);
+  Wire.setSDA(0);
+  Wire.setSCL(1);
   Wire.begin();
+  // Wire.setClock(50000);
   // Wait for serial port to connect
   while (!Serial) {
     delay(10);
@@ -45,34 +49,47 @@ void setup() {
   
   // Initialize ADS1015 at default address 0x48
   if (!ads.begin(0x48)) {
-    Serial.println("Failed to initialize ADS1015. Check wiring!");
+    Serial.println("Failed to initialize ADS1015 0x48. Check wiring!");
     while (1);
   }
   
+  // if (!ads1.begin(0x49)) {
+  //   Serial.println("Failed to initialize ADS1015 0x49. Check wiring!");
+  //   while (1);
+  // }
   // Set gain to ±4.096V range
-  ads.setGain(GAIN_ONE);
+  // ads.setGain(GAIN_ONE);
+  // ads1.setGain(GAIN_ONE);
   
   Serial.println("ADS1015 initialized successfully");
   Serial.println();
 }
 
 void loop() {
-  int16_t AH_raw = ads.readADC_SingleEnded(3);
+  
+  int16_t BH_raw = ads1.readADC_SingleEnded(1);
+  int16_t BL_raw = ads1.readADC_SingleEnded(0);
+  int16_t AH_raw = ads.readADC_SingleEnded(1);
+  int16_t AL_raw = ads.readADC_SingleEnded(0);
+  int16_t CH_raw = ads1.readADC_SingleEnded(2);
+  int16_t CL_raw = ads1.readADC_SingleEnded(3);
+  
   
   // Convert to temperature
   float AH = adcToTemp(AH_raw);
+  float AL = adcToTemp(AL_raw);
+  float BL = adcToTemp(BL_raw);
+  float BH = adcToTemp(BH_raw);
+  float CL = adcToTemp(CL_raw);
+  float CH = adcToTemp(CH_raw);
   
-  // Display results
-  Serial.print("Channel A1 - Raw ADC: ");
-  Serial.print(AH_raw);
-  Serial.print(" | Temperature: ");
-  
-  if (isnan(AH)) {
-    Serial.println("ERROR (Invalid reading)");
-  } else {
-    Serial.print(AH, 2);
-    Serial.println(" °C");
-  }
+Serial.print(AH); Serial.print(" ");
+Serial.print(AL); Serial.print(" ");
+Serial.print(BL); Serial.print(" ");
+Serial.print(BH); Serial.print(" ");
+Serial.print(CL); Serial.print(" ");
+Serial.println(CH);
+
   
   delay(500);
 }
